f3c7b6dd62652bd134b4d7252fc032cfea9d2fe2
aryan <aryan1bhokare@gmail.com>
2024-07-05 15:24:16 +0530

    added msg for reload
    Signed-off-by: aryan <aryan1bhokare@gmail.com>

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -1,3 +1,11 @@
 export default function DocsPage() {
-  return <></>;
+  return (
+    <main className="flex flex-col justify-center items-center h-full w-full  ">
+      <div>
+        <h1 className="text-white font-SpaceGrotesk font-bold text-3xl md:text-6xl mt-36 mb-12 ">
+          Please reload the page, this is a nextjs bug.
+        </h1>
+      </div>
+    </main>
+  );
 }
